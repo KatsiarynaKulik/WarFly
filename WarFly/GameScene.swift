7fea9d6732c99d51cd9634231a2601e351048846
Katsiaryna Kulik <125984123+KatsiarynaKulik@users.noreply.github.com>
2023-07-27 15:51:19 +0400

    Adding islands

--- WarFly/GameScene.swift
+++ WarFly/GameScene.swift
@@ -10,13 +10,23 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
+
     override func didMove(to view: SKView) {
 
-      let screenCenterPoint = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
-      let background = Background.populateBackground(at: screenCenterPoint)
-      background.size = self.size
-      self.addChild(background)
+        let screenCenterPoint = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
+        let background = Background.populateBackground(at: screenCenterPoint)
+        background.size = self.size
+        self.addChild(background)
 
+        let screen = UIScreen.main.bounds
+        for _ in 1...5 {
+
+            let x: CGFloat = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound: Int(screen.size.width)))
+            let y: CGFloat = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound: Int(screen.size.height)))
+
+            let island = Island.populateIsland(at: CGPoint(x: x, y: y))
+            self.addChild(island)
+        }
     }
 }
+
---
+++ WarFly/Island.swift
@@ -0,0 +1,46 @@
+//
+//  Island.swift
+//  WarFly
+//
+//  Created by Katsiaryna Kulik  on 27.07.23.
+//  Copyright © 2023 Katsiaryna Kulik. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class Island: SKSpriteNode {
+    static func populateIsland(at point: CGPoint) -> Island {
+        let islandImageName = configureIslandName()
+        let island = Island(imageNamed: islandImageName)
+        island.setScale(randomScaleFactor)
+        island.position = point
+        island.zPosition = 1
+        island.run(rotateForRandomAngle())
+
+        return island
+
+    }
+
+    static func configureIslandName() -> String {
+        let distribution = GKRandomDistribution(lowestValue: 1, highestValue: 4)
+        let randomNumber = distribution.nextInt()
+        let imageName = "is" + "\(randomNumber)"
+
+        return imageName
+    }
+
+    static var randomScaleFactor: CGFloat {
+        let distribution = GKRandomDistribution(lowestValue: 1, highestValue: 10)
+        let randomNumber = CGFloat(distribution.nextInt()) / 10
+
+        return randomNumber
+    }
+
+    static func rotateForRandomAngle() -> SKAction {
+        let distribution = GKRandomDistribution(lowestValue: 0, highestValue: 360)
+        let randomNumber = CGFloat(distribution.nextInt())
+
+        return SKAction.rotate(toAngle: randomNumber * CGFloat(Double.pi / 180), duration: 0)
+    }
+}
