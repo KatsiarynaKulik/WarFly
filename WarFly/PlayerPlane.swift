4c1147719fa7a37cab141a25f694a65c26f30f1d
Katsiaryna Kulik <125984123+KatsiarynaKulik@users.noreply.github.com>
2023-08-09 23:16:56 +0400

    Add turning plane animation

--- WarFly/PlayerPlane.swift
+++ WarFly/PlayerPlane.swift
@@ -11,10 +11,13 @@ import CoreMotion
 
 class PlayerPlane: SKSpriteNode {
 
-  let motionManager = CMMotionManager()
-  var xAcceleration: CGFloat = 0
-  let screenSice = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
-  
+    let motionManager = CMMotionManager()
+    var xAcceleration: CGFloat = 0
+    let screenSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+    var leftTextureArrayAnimation = [SKTexture]()
+    var rightTextureArrayAnimation = [SKTexture]()
+    var forwardTextureArrayAnimation = [SKTexture]()
+
     static func populate(at point: CGPoint) -> PlayerPlane {
         let playerPlaneTexture = SKTexture(imageNamed: "airplane_3ver2_13")
         let playerPlane = PlayerPlane(texture: playerPlaneTexture)
@@ -24,25 +27,108 @@ class PlayerPlane: SKSpriteNode {
         return playerPlane
     }
 
-  func checkPosition() {
-    self.position.x += xAcceleration * 50
+    func checkPosition() {
+        self.position.x += xAcceleration * 50
 
-    if self.position.x < -70 {
-      self.position.x = screenSice.width + 70
-    } else if self.position.x > screenSice.width + 70 {
-      self.position.x = -70
-    }
-
-  }
-
-  func performFly() {
-
-    motionManager.accelerometerUpdateInterval = 0.2
-    motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (data, error) in
-        if let data = data {
-            let acceleration = data.acceleration
-            self.xAcceleration = CGFloat(acceleration.x) * 0.7 + self.xAcceleration * 0.3
+        if self.position.x < -70 {
+            self.position.x = screenSize.width + 70
+        } else if self.position.x > screenSize.width + 70 {
+            self.position.x = -70
         }
     }
-  }
+
+    func performFly() {
+
+        planeAnimationFillArray()
+        motionManager.accelerometerUpdateInterval = 0.2
+        motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { (data, error) in
+            if let data = data {
+                let acceleration = data.acceleration
+                self.xAcceleration = CGFloat(acceleration.x) * 0.7 + self.xAcceleration * 0.3
+            }
+        }
+    }
+
+    fileprivate func planeAnimationFillArray() {
+
+        SKTextureAtlas.preloadTextureAtlases([SKTextureAtlas(named: "PlayerPlane")]) {
+
+            self.leftTextureArrayAnimation = {
+
+                var array = [SKTexture]()
+                for i in stride(from: 13, through: 1, by: -1) {
+                    let number = String(format: "%02d", i)
+                    let texture = SKTexture(imageNamed: "airplane_3ver2_\(number)")
+                    array.append(texture)
+                }
+
+                SKTexture.preload(array, withCompletionHandler: {
+                    print("preload is done")
+                })
+                return array
+
+            }()
+
+            self.rightTextureArrayAnimation = {
+
+                var array = [SKTexture]()
+                for i in stride(from: 13, through: 26, by: 1) {
+                    let number = String(format: "%02d", i)
+                    let texture = SKTexture(imageNamed: "airplane_3ver2_\(number)")
+                    array.append(texture)
+                }
+
+                SKTexture.preload(array, withCompletionHandler: {
+                    print("preload is done")
+                })
+                return array
+
+            }()
+
+            self.forwardTextureArrayAnimation = {
+
+                var array = [SKTexture]()
+                let texture = SKTexture(imageNamed: "airplane_3ver2_13")
+                array.append(texture)
+
+
+                SKTexture.preload(array, withCompletionHandler: {
+                    print("preload is done")
+                })
+                return array
+
+            }()
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
