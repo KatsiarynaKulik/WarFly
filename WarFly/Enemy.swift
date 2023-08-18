2fa31a038e6ea43942b16fee8557acf836ca9c6a
Katsiaryna Kulik <125984123+KatsiarynaKulik@users.noreply.github.com>
2023-08-18 23:46:28 +0400

    Add enemy final movement

--- WarFly/Enemy.swift
+++ WarFly/Enemy.swift
@@ -11,9 +11,10 @@ import SpriteKit
 class Enemy: SKSpriteNode {
 
     static var textureAtlas: SKTextureAtlas?
+    var enemyTexture: SKTexture!
 
-    init() {
-        let texture = Enemy.textureAtlas?.textureNamed("airplane_4ver2_13")
+    init(enemyTexture: SKTexture) {
+        let texture = enemyTexture
         super.init(texture: texture, color: .clear, size: CGSize(width: 221, height: 204))
         self.xScale = 0.5
         self.yScale = -0.5
@@ -21,25 +22,57 @@ class Enemy: SKSpriteNode {
         self.name = "sprite"
     }
 
-  func flySpiral() {
-    let screenSice = UIScreen.main.bounds
-    let timeHorizontal: Double = 3
-    let timeVertical: Double = 10
-    let moveLeft = SKAction.moveTo(x: 50, duration: timeHorizontal)
-    moveLeft.timingMode = .easeInEaseOut
-    let moveRight = SKAction.moveTo(x: screenSice.width - 50, duration: timeHorizontal)
-    moveRight.timingMode = .easeInEaseOut
+    func flySpiral() {
 
-    let asideMovementCenter = SKAction.sequence([moveLeft, moveRight])
-    let foreverAsideMovement = SKAction.repeatForever(asideMovementCenter)
-    let forwardMovement = SKAction.moveTo(y: -105, duration: timeVertical)
-    let groupMovement = SKAction.group([foreverAsideMovement, forwardMovement])
-    self.run(groupMovement)
+        let screenSize = UIScreen.main.bounds
 
+        let timeHorizontal: Double = 3
+        let timeVertical: Double = 5
 
-  }
+        let moveLeft = SKAction.moveTo(x: 50, duration: timeHorizontal)
+        moveLeft.timingMode = .easeInEaseOut
+        let moveRight = SKAction.moveTo(x: screenSize.width - 50, duration: timeHorizontal)
+        moveRight.timingMode = .easeInEaseOut
+
+        let randomNumber = Int(arc4random_uniform(2))
+
+        let asideMovementSequence = randomNumber == EnemyDirection.left.rawValue ? SKAction.sequence([moveLeft, moveRight]) : SKAction.sequence([moveRight, moveLeft])
+
+        let foreverAsideMovement = SKAction.repeatForever(asideMovementSequence)
+
+        let forwardMovement = SKAction.moveTo(y: -105, duration: timeVertical)
+        let groupMovement = SKAction.group([foreverAsideMovement, forwardMovement])
+        self.run(groupMovement)
+    }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
+
+enum EnemyDirection: Int {
+    case left = 0
+    case right
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- WarFly/GameScene.swift
+++ WarFly/GameScene.swift
@@ -9,6 +9,7 @@
 import SpriteKit
 import GameplayKit
 
+
 class GameScene: SKScene {
 
     var player: PlayerPlane!
@@ -24,7 +25,8 @@ class GameScene: SKScene {
         }
 
         spawnPowerUp()
-        spawnEnemy(count: 5)
+//        spawnEnemy(count: 5)
+        spawnEnemies()
     }
 
     fileprivate func spawnPowerUp() {
@@ -34,23 +36,40 @@ class GameScene: SKScene {
         self.addChild(powerUp)
     }
 
-    fileprivate func spawnEnemy(count: Int) {
-        let enemyTextureAtlas = SKTextureAtlas(named: "Enemy_1")
-        SKTextureAtlas.preloadTextureAtlases([enemyTextureAtlas]) {
-            Enemy.textureAtlas = enemyTextureAtlas
+
+    fileprivate func spawnEnemies() {
+        let waitAction = SKAction.wait(forDuration: 3.0)
+        let spawnSpiralAction = SKAction.run { [unowned self] in
+            self.spawnSpiralOfEnemies()
+        }
+
+        self.run(SKAction.repeatForever(SKAction.sequence([waitAction, spawnSpiralAction])))
+    }
+
+
+    fileprivate func spawnSpiralOfEnemies() {
+        let enemyTextureAtlas1 = SKTextureAtlas(named: "Enemy_1")
+        let enemyTextureAtlas2 = SKTextureAtlas(named: "Enemy_2")
+        SKTextureAtlas.preloadTextureAtlases([enemyTextureAtlas1, enemyTextureAtlas2]) { [unowned self] in
+
+            let randomNumber = Int(arc4random_uniform(2))
+            let arrayOfAtlases = [enemyTextureAtlas1, enemyTextureAtlas2]
+            let textureAtlas = arrayOfAtlases[randomNumber]
+
             let waitAction = SKAction.wait(forDuration: 1.0)
-            let spawnEnemy = SKAction.run({
-                let enemy = Enemy()
+            let spawnEnemy = SKAction.run({ [unowned self] in
+                let textureNames = textureAtlas.textureNames.sorted()
+                let texture = textureAtlas.textureNamed(textureNames[12])
+                let enemy = Enemy(enemyTexture: texture)
                 enemy.position = CGPoint(x: self.size.width / 2, y: self.size.height + 110)
                 self.addChild(enemy)
                 enemy.flySpiral()
             })
 
             let spawnAction = SKAction.sequence([waitAction, spawnEnemy])
-            let repeatAction = SKAction.repeat(spawnAction, count: count)
+            let repeatAction = SKAction.repeat(spawnAction, count: 3)
             self.run(repeatAction)
         }
-
     }
 
     fileprivate func spawnClouds() {
@@ -93,8 +112,6 @@ class GameScene: SKScene {
 
         player = PlayerPlane.populate(at: CGPoint(x: screen.size.width / 2, y: 100))
         self.addChild(player)
-
-
     }
 
     override func didSimulatePhysics() {
