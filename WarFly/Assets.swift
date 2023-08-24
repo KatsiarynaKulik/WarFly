d4717071b719dcc49c3aef47f8b5637d8cdaf684
Katsiaryna Kulik <125984123+KatsiarynaKulik@users.noreply.github.com>
2023-08-24 23:50:33 +0400

    Add preload atlases

---
+++ WarFly/Assets.swift
@@ -0,0 +1,22 @@
+//
+//  Assets.swift
+//  WarFly
+//
+//  Created by Katsiaryna Kulik  on 24.08.23.
+//  Copyright © 2023 Katsiaryna Kulik. All rights reserved.
+//
+
+import SpriteKit
+
+class Assets {
+
+  static let sharedd = Assets()
+  let yellowAmmo = SKTextureAtlas(named: "yellowAmmo")
+  let greenPowerUp = SKTextureAtlas(named: "greenPowerUp")
+  let bluePowerUp = SKTextureAtlas(named: "bluePowerUp")
+  let enemy_2 = SKTextureAtlas(named: "enemy_2")
+  let enemy_1 = SKTextureAtlas(named: "enemy_1")
+  let playerPlane = SKTextureAtlas(named: "playerPlane")
+
+
+}
---
+++ WarFly/YellowShot.swift
@@ -0,0 +1,21 @@
+//
+//  YellowShow.swift
+//  WarFly
+//
+//  Created by Katsiaryna Kulik  on 23.08.23.
+//  Copyright © 2023 Katsiaryna Kulik. All rights reserved.
+//
+
+import SpriteKit
+
+class YellowShot: Shot {
+
+    init() {
+        let textureAtlas = SKTextureAtlas(named: "YellowShot")
+        super.init(textureAtlas: textureAtlas)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
