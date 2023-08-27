25ecd58819182833861d360867429adc03e3f1c6
Katsiaryna Kulik <125984123+KatsiarynaKulik@users.noreply.github.com>
2023-08-28 02:20:32 +0400

    Add user Interface

--- WarFly/GameScene.swift
+++ WarFly/GameScene.swift
@@ -13,6 +13,12 @@ import GameplayKit
 class GameScene: SKScene {
 
     var player: PlayerPlane!
+  let scoreBacground = SKSpriteNode(imageNamed: "scores")
+  let scoreLabel = SKLabelNode(text: "10000")
+  let menuButton = SKSpriteNode(imageNamed: "menu")
+  let life1 = SKSpriteNode(imageNamed: "life")
+  let life2 = SKSpriteNode(imageNamed: "life")
+  let life3 = SKSpriteNode(imageNamed: "life")
 
     override func didMove(to view: SKView) {
 
@@ -22,16 +28,42 @@ class GameScene: SKScene {
         configureStartScene()
         spawnClouds()
         spawnIslands()
-        let deadline = DispatchTime.now() + .nanoseconds(1)
-        DispatchQueue.main.asyncAfter(deadline: deadline) { [unowned self] in
             self.player.performFly()
-        }
 
         spawnPowerUp()
         spawnEnemies()
+      configureUI()
     }
 
-    fileprivate func spawnPowerUp() {
+  fileprivate func configureUI() {
+    scoreBacground.position = CGPoint(x: scoreBacground.size.width + 10, y: self.size.height - scoreBacground.size.height / 2 - 10)
+    scoreBacground.anchorPoint = CGPoint(x: 1.0, y: 0.5)
+    scoreBacground.zPosition = 99
+    addChild(scoreBacground)
+
+    scoreLabel.horizontalAlignmentMode = .right
+    scoreLabel.verticalAlignmentMode = .center
+    scoreLabel.position = CGPoint(x: -10, y: 3)
+    scoreLabel.zPosition = 100
+    scoreLabel.fontName = "AmericanTypewriter-Bold"
+    scoreLabel.fontSize = 30
+    scoreBacground.addChild(scoreLabel)
+
+    menuButton.position = CGPoint(x: 20, y: 20)
+    menuButton.anchorPoint = CGPoint(x: 0.0, y: 0.0)
+    menuButton.zPosition = 100
+    addChild(menuButton)
+
+    let lifes = [life1, life2, life3]
+    for (index, life) in lifes.enumerated() {
+      life.position = CGPoint(x: self.size.width - CGFloat(index + 1) * (life.size.width + 3), y: 30)
+      life.zPosition = 100
+      life.anchorPoint = CGPoint(x: 0.0, y: 0.0)
+      addChild(life)
+    }
+  }
+
+  fileprivate func spawnPowerUp() {
 
         let spawnAction = SKAction.run {
             let randomNumber = Int(arc4random_uniform(2))
